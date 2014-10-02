7c57641b34a160b929a68744657400dfd40657dd
Emil Tullstedt <emiltu@kth.se>
2014-10-02 16:56:19 +0200

    Added flight

--- src/index.php
+++ src/index.php
@@ -1,46 +1,31 @@
-<!doctype html>
-<html>
-<head>
-    <link href="css/main.css" rel="stylesheet" type="text/css" />
-    <link href="fonts/fontsheet.css" rel="stylesheet" type="text/css" />
-    <meta charset="utf-8" />
-    <title>Tasty Recipes</title>
+<?php
 
-    <script src="js/jquery.js"></script>
-    <script src="js/main.js"></script>
-</head>
+require_once("flight/flight/Flight.php");
 
-<body>
-    <div id="container">
-        <header>
-            <h1>Tasty Recipes</h1>
-        </header>
-        <nav>
-            <a href="index.html" class="current">Main</a>
-            <a href="calendar.html">Calendar</a>
-            <a href="recipes.html" id="nav_recipe">Recipes</a>
-            <div id="dropdown_recipe" class="dropdown">
-                <a href="pancakes.html">
-                    Pancakes
-                </a>
-                <a href="meatballs.html">
-                    Meatballs!
-                </a>
-            </div>
-            <a href="contact.html" id="nav_contact">Contact</a>
-            <div id="dropdown_contact" class="dropdown">
-                <a href="contact_us.html">Contact us</a>
-                <a href="about_us.html">About us</a>
-            </div>
-        </nav>
+Flight::route('/', function(){
+    Flight::render('index.php');
+});
 
-        <section>
-            <h2>Tasty Recipes</h2>
+Flight::route('/calendar', function(){
+    Flight::render('calendar.php');
+});
 
-            Welcome to the recipe site for a new generation of food lovers!
-        </section>
-        <footer>Developed by Emil Tullstedt for ID1354</footer>
-    </div>
-</body>
-</html>
+Flight::route('/recipes', function(){
+    Flight::render('recipes.php');
+});
 
+Flight::route('/company', function(){
+    Flight::render('contact.php');
+});
+
+Flight::route('/company/about', function(){
+    Flight::render('about_us.php');
+});
+
+Flight::route('/company/contact', function(){
+    Flight::render('contact_us.php');
+});
+
+Flight::start();
+
+?>
---
+++ src/views/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    <link href="fonts/fontsheet.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8" />
+    <title>Tasty Recipes</title>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <h1>Tasty Recipes</h1>
+        </header>
+        <nav>
+            <a href="index.html" class="current">Main</a>
+            <a href="calendar.html">Calendar</a>
+            <a href="recipes.html" id="nav_recipe">Recipes</a>
+            <div id="dropdown_recipe" class="dropdown">
+                <a href="pancakes.html">
+                    Pancakes
+                </a>
+                <a href="meatballs.html">
+                    Meatballs!
+                </a>
+            </div>
+            <a href="contact.html" id="nav_contact">Contact</a>
+            <div id="dropdown_contact" class="dropdown">
+                <a href="contact_us.html">Contact us</a>
+                <a href="about_us.html">About us</a>
+            </div>
+        </nav>
+
+        <section>
+            <h2>Tasty Recipes</h2>
+
+            Welcome to the recipe site for a new generation of food lovers!
+        </section>
+        <footer>Developed by Emil Tullstedt for ID1354</footer>
+    </div>
+</body>
+</html>
+
